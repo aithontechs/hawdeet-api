af0206054695ca4032a564ae2a918f50712d0121
Mahmoud Abdelrahim <abdelrahimmahmoud6@gmail.com>
2026-06-05 17:45:59 +0300

    Update Webhook of paymob test 2

--- app/Http/Controllers/Application/Payment/PaymentController.php
+++ app/Http/Controllers/Application/Payment/PaymentController.php
@@ -32,6 +32,29 @@ class PaymentController extends Controller
             'body'  => $request->all(),
         ]);
 
+        Log::info('HMAC Fields', [
+            'amount_cents'            => $obj['amount_cents'] ?? 'NULL',
+            'created_at'              => $obj['created_at'] ?? 'NULL',
+            'currency'                => $obj['currency'] ?? 'NULL',
+            'error_occured'           => $obj['error_occured'] ?? 'NULL',
+            'has_parent_transaction'  => $obj['has_parent_transaction'] ?? 'NULL',
+            'id'                      => $obj['id'] ?? 'NULL',
+            'integration_id'          => $obj['integration_id'] ?? 'NULL',
+            'is_3d_secure'            => $obj['is_3d_secure'] ?? 'NULL',
+            'is_auth'                 => $obj['is_auth'] ?? 'NULL',
+            'is_capture'              => $obj['is_capture'] ?? 'NULL',
+            'is_refunded'             => $obj['is_refunded'] ?? 'NULL',
+            'is_standalone_payment'   => $obj['is_standalone_payment'] ?? 'NULL',
+            'is_voided'               => $obj['is_voided'] ?? 'NULL',
+            'order_id'                => $obj['order']['id'] ?? 'NULL',
+            'owner'                   => $obj['owner'] ?? 'NULL',
+            'pending'                 => $obj['pending'] ?? 'NULL',
+            'success'                 => $obj['success'] ?? 'NULL',
+            'source_data_pan'         => $obj['source_data']['pan'] ?? 'NULL',
+            'source_data_sub_type'    => $obj['source_data']['sub_type'] ?? 'NULL',
+            'source_data_type'        => $obj['source_data']['type'] ?? 'NULL',
+        ]);
+
         $hmac = $request->query('hmac');
         $obj  = $request->input('obj');
 
