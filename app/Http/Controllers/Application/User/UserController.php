be5b12bd3fbb85a1de90a402d310eaf05e001994
Mahmoud Abdelrahim <abdelrahimmahmoud6@gmail.com>
2026-07-27 11:47:22 +0300

    Update get of subscription in profile

--- app/Http/Controllers/Application/User/UserController.php
+++ app/Http/Controllers/Application/User/UserController.php
@@ -33,8 +33,8 @@ class UserController extends Controller
 
         $user->load([
             'subscriptions' => fn ($q) =>
-                $q->select(['id','user_id','start_at','end_at','price','status','payment_status','canceled_at','ended_reason' , 'plan_id'])
-                ->with('plan:id,name,duration_months,price,compare_price')->latest()->get(),
+                $q->where('status' , 'active')->where('payment_status' , 'paid')->select(['id','user_id','start_at','end_at','price','status','payment_status','canceled_at','ended_reason' , 'plan_id'])
+                ->with('plan:id,name,duration_months,price,compare_price')->latest()->limit(1),
             'authorBooks' => fn ($q) =>
                 $q->select(['id','author_id','title','cover','avg_rating'])
                 ->where('published', true),
