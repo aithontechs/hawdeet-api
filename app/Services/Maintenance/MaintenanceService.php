1df53badf54529d3ae1d04b561ada2ff3b9dd938
Mahmoud Abdelrahim <abdelrahimmahmoud6@gmail.com>
2026-07-29 18:28:41 +0300

    Run Schedule of project to expired coupons and subscriptions v3

--- app/Services/Maintenance/MaintenanceService.php
+++ app/Services/Maintenance/MaintenanceService.php
@@ -5,10 +5,13 @@ namespace App\Services\Maintenance;
 use App\Models\Coupon;
 use App\Models\Payment;
 use App\Models\UserSubscription;
+use App\Services\DashboardStats\DashboardService;
 use Illuminate\Support\Facades\DB;
 
 class MaintenanceService
 {
+    public function __construct(private readonly DashboardService $dashboardService){}
+
     public function expireSubscriptions(): void
     {
         UserSubscription::query()
@@ -50,6 +53,7 @@ class MaintenanceService
                         ->delete();
 
             UserSubscription::whereIn('id', $ids)->delete();
+            $this->dashboardService->clearCache() ;
         });
     }
 }
